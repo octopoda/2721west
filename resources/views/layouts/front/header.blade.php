f05804a003d0c710b6566f8bec7799c4f4771be7
Zack Davis <zack@octopodainteractive.com>
2015-09-26 17:51:03 -0500

    trying env variables

--- resources/views/layouts/front/header.blade.php
+++ resources/views/layouts/front/header.blade.php
@@ -65,9 +65,9 @@
 
   
   <!-- CSS: implied media="all" -->
-  @if ($app->enviroment('local'))
+  @if ($app->environment('local'))
     <link rel="stylesheet" href="assets/css/app.min.css">
-  @elseif ($app->enviroment('production')) 
+  @elseif ($app->environment('production')) 
     <link rel="stylesheet" href="https://s3.amazonaws.com/2721west-assets/css/app.min.css">
   @endif
 
