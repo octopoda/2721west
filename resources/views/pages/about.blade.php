e55500922b6b3bd2d641ce3f0500a7170fc44900
Zack Davis <zack@octopodainteractive.com>
2015-10-09 16:37:04 -0500

    added resume

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,7 @@
 	<article class="about__wrapper" data-ng-controller="AboutController as vm">
 		<div class="row">
 			<section class="about__intro">
-				<h1> I am a digital designer and a full stack developer. Someone who will work 
+				<h1> I am a digital designer, strategist, and a full-stack developer. Someone who will work 
 					<span data-browser-detect class="malarkey" ng-click="vm.type()">
 						<span class="malarkey--hover"></span>
                 		<span class="malarkey--text" id="malarkey">smart</span>
@@ -15,13 +15,21 @@
 				<p>I understand what it takes to design and develop an application from the ground up and am experienced in business practices, data management, systems administration, server side coding, and front end coding.</p>
 				<p>My passion is building delightful and engaging digital experiences that draw the user into meeting the company&rsquo;s goals.  </p>
 				<p><a data-modal-toggle data-target="contactModal">Contact me</a> and let&rsquo;s talk.</p>
+
+
 			</section>
 		</div>
 
 		<div class="row">
 			<div class="about-resume">
 				@include('layouts.front.resume')
+
+				<ul class="about__more">
+              		<li><a href="https://s3.amazonaws.com/2721west-assets/documents/zack-davis-resume.pdf" id="resumeDownload">Download My Resume<i class="fa fa-angle-right"></i></li>
+            	</ul>
 			</div>
+
+			
 		</div>
 	</article>
 @endsection
