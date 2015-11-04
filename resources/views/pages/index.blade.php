9048814f5603f9307d74c1019f23ed4718968dce
Zack Davis <zack@octopodainteractive.com>
2015-11-04 15:05:09 -0600

    new front page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,9 +12,7 @@
       <div class="row">
        	<section data-into class="home__main into">
             <h1>I am Zack Davis, a <br>
-              <span class="malarkey" ng-click="vm.type()">
-                <span class="malarkey--text home--malarkey" id="malarkey">Creative Director.</span>
-              </span><br>
+              <span class="malarkey--text home--malarkey">Senior Product Design and Developer</span>
               I am a problem solver. A doer.  Someone <wbr> that 
               can wear any hat necessary to complete the job.  
             </h1>
