fb6533079f81a6776f596ec568d216eb96b8d7c5
Zack Davis <zack@octopodainteractive.com>
2016-04-01 10:09:11 -0500

    added namegame

---
+++ resources/views/google/namegame.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html data-ng-app="willowtree" data-ng-strict-di>
+  <head>
+    <meta charset="utf-8">
+    <title fill-title></title>
+    <meta name="description" content="Name Game for Google">
+    <meta name="viewport" content="width=device-width">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <base>
+    
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <link rel="stylesheet" href="/assets/css/vendor-6c91c750c5.css">
+    <link rel="stylesheet" href="/assets/css/app-e8869fed52.css">
+  </head>
+
+  <body  class="no-js">
+    <!--[if lt IE 10]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    
+    <div ui-view></div>
+    <div class="building-background__wrapper">
+        <div class="building-background__image"></div>
+    </div>
+    <script src="/assets/scripts/vendor-0eb61fe301.js"></script>
+    <script src="/assets/scripts/app-4dadf34e5f.js"></script>
+
+  </body>
+</html>
