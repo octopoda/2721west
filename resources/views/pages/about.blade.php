cef9987b6887d30dbd57187d315cb4de3e5344b5
Zack Davis <zack@octopodainteractive.com>
2016-04-17 15:33:07 -0500

    added capturely to it

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,14 +4,10 @@
 	<article class="about__wrapper" data-ng-controller="AboutController as vm">
 		<div class="row">
 			<section class="about__intro">
-				<h1> I am a UX leader, a digital strategist, and a full-stack developer. Someone who will work 
-					<span data-browser-detect class="malarkey" ng-click="vm.type()">
-						<span class="malarkey--text" id="malarkey">smart</span>
-                	</span>to get the job done. </h1>
+				<h1> Like I said, title are worthless.  I love UX and I am someone tht will work smart to get the job done. </h1>
 				
-				<p>I have over 13 years experience in the design industry and nine years of experience in start-ups. I created Octopoda Interactive as my own digital boutique agency to give those start-ups the necessary footing needed to create a successful business.</p>  
-				<p>I understand what it takes to design and develop an application end to end and am experienced in all sides of the application launch.</p>
-				<p>My passion is building delightful and engaging digital experiences that draw the user into meeting the company&rsquo;s goals.  </p>
+				<p>I am a UX designer, digital strategist, and full-stack developer with 13 years of experience in the design/finance industry 9 years of that time has been focused on providing branding, design, and development services to start-ups and small businesses through my boutique agency.</p>
+				<p>I am passionate about building beautiful and engaging digital experiences that draw in users and exceed client expectations, My expertise lies in understanding and executing everything end-to-end, from visual brands and corporate identities through the integration of design, digital strategy, and full-stack development.</p>
 				
 
 
@@ -20,7 +16,7 @@
               		<li><a target="_blank" href="https://s3.amazonaws.com/2721west-assets/documents/zack-davis-resume.pdf" id="resumeDownload">Download My Resume<i class="fa fa-angle-right"></i></a></li>
               		<li><a target="_blank" href="https://www.linkedin.com/in/zackdavis" id="linkedIn">Linked In <i class="fa fa-angle-right"></i></a></li>
               		<li><a target="_blank" href="https://twitter.com/2721west" id="twitter">Twitter<i class="fa fa-angle-right"></i></a></li>
-              		<li><a target="_blank" href="https://dribbble.com/octopoda" id="twitter">Dribbble<i class="fa fa-angle-right"></i></a></li>
+              		<li><a target="_blank" href="https://dribbble.com/octopoda" id="dribbble">Dribbble<i class="fa fa-angle-right"></i></a></li>
             	</ul>
 
 			</section>
