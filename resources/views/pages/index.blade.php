df2a2410fa3b138b906600510001b315db8a05c9
Zack Davis <zack@octopodainteractive.com>
2017-04-10 21:52:35 -0500

    chaning homepage back

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,14 +11,14 @@
 
        <div class="row">
         <section data-into class="home__main into">
-            <h1>I am Zack Davis, an <br>
-             <a href="{{ route('about') }}" ><span class="malarkey--text home--malarkey">UX Lead @ AssetBuilder Inc.</span></a>
+            <h1>I am Zack Davis,  <br>
+             UX Lead at AssetBuilder Inc.
              
             </h1>
             
             
             <ul class="home__more">
-              <li><a href="{{ route('about') }}" id="homeLearn">Learn More About Me <i class="fa fa-angle-right"></i></a></li>
+              <li style="list-style:none"><a href="{{ route('about') }}" id="homeLearn">Learn More About Me <i class="fa fa-angle-right"></i></a></li>
               {{-- <li><a data-modal-toggle data-target="workModal" class="laptop-up">View My Work <i class="fa fa-angle-right"></i></a> </li> --}}
             </ul>
           </section>
